de3b42784dc5cfff02eef51f1421f703dc0eb6c2
mol1986@naver.com <kileab@naver.com>
2022-09-02 21:47:17 +0900

    2022.9.2 (구조체)

---
+++ Function.h
@@ -0,0 +1,4 @@
+int Function(int x, int y)
+{
+	return x + y;
+}
--- Source.c
+++ Source.c
@@ -1,73 +1,94 @@
-﻿#include <stdio.h>
+﻿#include <stdio.h> // <> 라이브러리 헤더 파일을 가져올 때 사용합니다.
+#include "Function.h" // "" 사용자 정의 헤더 파일을 가져올 때 사용합니다.
+
+// 매크로란?
+/*
+// 프로그램 내에서 특정한 데이터가 문자열로 정의되고 처리되는 과정입니다.
+
+// 매크로는 자료형이 없으므로 메모리 공간이 확보되지 않습니다.
+#define PI 3.14
+
+// 매크로에서는 ;로 필요하지 않습니다.
+
+// 매크로 함수도 자료형이 없으므로 메모리 공간이 확보되지 않습니다.
+#define Solution(x, y) x * y // 1 + (1 * 2) + 1
+
+#define ANDROID 1
+#define IOS 0
+*/
+
+// 구조체
+struct Character
+{
+	int health;
+	float weight;
+	const char * name;
+	// 구조체를 선언하기 전에 구조체는 
+	// 메모리 공간이 생성되지 않으므로,
+	// 구조체 내부에 있는 데이터를 초기화할 수 없습니다.
+};
 
 void main()
 {
-	// 포인터 배열이란?
+	// 전처리기란?
+/*
+	// 프로그램이 컴파일되기 이전에 프로그램에 대한 사전 처리하는 과정입니다.
+	int result = Function(10, 20);	
+
+	printf("result 변수의 값 : %d\n", result);
+
+	// 매크로 변수는 상수이고, 메모리 공간이 없기 때문에 값을 변경할 수 없습니다.
+	// PI = 3.45;
+
+	int a = 1;
+	int b = 2;
+
+	printf("PI의 값 : %f\n", PI);
+	printf("Solution 함수의 결과 : %d\n", Solution(a,b));
+	printf("Solution 함수의 결과 : %d\n", Solution(a + 1, b + 1));
+*/
+
+	// 조건부 컴파일
+/*
+	// 조건에 따라 코드의 일정 부분을 컴파일할지 안 할지 결정할 수 있습니다.
+	// 조건부 컴파일은 #endif를 사용해서 끝내야 됩니다.
+#if IOS
+	// PC에 알맞는 키 입력
+	printf("안드로이드 기기입니다.");
+#elif ANDROID
+	// 모바일에 알맞는 키 입력
+	printf("두 번째 조건입니다.");
+#else
+	printf("2개의 조건이 다 틀립니다.");
+#endif
+*/
+
+	// 구조체란?
 	/*
-	// 배열의 요소로 포인터 변수를 가지는 배열입니다.
-	int num1 = 10, num2 = 20, num3 = 30;
+	// 여러 개의 변수를 하나의 집합으로 구조화한
+	// 다음 하나의 객체를 생성하는 것입니다.
+	struct Character leesin;
 
-	int * array[3] = { &num1, &num2, &num3 };
+	leesin.health = 100;
+	leesin.name = "Leesin";
+	leesin.weight = 80.5f;
 
-	for (int i = 0; i < 3; i++)
-	{
-		printf("array 포인터의 값 : %p\n", array[i]);
-		printf("array 포인터가 가리키는 값 : %d\n", *array[i]);
-	}
-                  
-	const char * data[3] = { "Apple", "Banana", "Melon" };
+	printf("leesin의 체력 : %d\n", leesin.health);
+	printf("leesin의 이름 : %s\n", leesin.name);
+	printf("leesin의 몸무게 : %f\n", leesin.weight);	
 
-	// 새로운 KImgeumsoo라는 문자열의 시작 주소를 다시 가라킵니다.
-	// 그리고 이전에 참조되어 있던 Apple의 참조는 끊깁니다.
-	data[0] = "Kimgeumsoo";
+	// 구조체에 초기화 리스트를 사용할 때는 
+	// 구조체 위에서 선언된 변수의 순서로 정의되어야 합니다.
+	struct Character Alistar = {200, 100.5, "Alistar"};
 
-	// *data[0] = 'A';
-
-	for (int i = 0; i < 3; i++)
-	{
-		// %s : '\0'문자를 만나는 순간까지 계속 출력하는 서식 지정자입니다.
-		printf("%s\n", data[i]);
-	}
+	printf("leesin의 체력 : %d\n", Alistar.health);
+	printf("leesin의 이름 : %s\n", Alistar.name);
+	printf("leesin의 몸무게 : %f\n", Alistar.weight);
 	*/
 
-	// ASCII 코드란?
-	/*
-	// 미국 표준화 협회가 제정한 정보 교환용 표준 코드로 영문 알파벳을
-	// 사용하는 대표적인 문자 인코딩입니다.
-	if ('A' < 'B')
-	{
-		for (int i = 0; i < 26; i++)
-		{
-			char alphabet = 'A' + i;
-			printf("%c\n", alphabet);
-		}
-	}
-	*/
-
-	// 공약수
-	/*
-	// (int) 두 개의 변수를 입력받습니다.
-	// A 변수와 B 변수의 공통된 약수를 출력해주세요.
-	int A, B;
-
-	scanf_s("%d%d", &A, &B);
-
-	for (int i = 1; i <= A && i <= B; i++)
-	{
-		if (A % i == 0 && B % i == 0)
-		{
-			printf("%d  ", i);
-		}
-	}
-	*/
-
-	// 자기가 스스로 생각하고 프로그램을 만들어보는게 실력이 많이 향상됩니다.
-	// 혼자서 프로그램을 만드는 거보다 팀을 만들어서 간단한 프로젝트를 추천합니다.
-    
-	// 짝수 완전수
-	// 자기 자신을 제외한 양의 약수(진약수)를 더했을 때 
-	// 자기 자신이 되는 양의 정수를 말한다.
-
+	// 과잉수
+	// 자연수 중에서 자기 자신을 제외한 양의 약수를
+	// 모두 더했을 때 자기 자신보다 더 커지는 수이다.
 	int value;
 	int result = 0;
 
@@ -81,15 +102,14 @@ void main()
 		}
 	}
 
-	if (value == result)
+	if (result > value)
 	{
-		printf("완전수");
+		printf("과잉수");
 	}
 	else
 	{
-		printf("완전수가 아닙니다.");
+		printf("과잉수가 아닙니다.");
 	}
 
-	// 6 입력 -> 완전수입니다.
-	// 7 입력 -> 완전수아닙니다.
 }
+
