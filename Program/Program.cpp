d6bdaea1be6901c1b77b80986956be76bfb05d3f
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-07-16 21:44:42 +0900

    (2025.7.16) 이중 연결 리스트

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -62,6 +62,79 @@ public:
 		}
 	}
 
+	void push_back(T data)
+	{
+		Node * newNode = new Node;
+
+		newNode->data = data;
+		newNode->next = nullptr;
+
+		if (head == nullptr)
+		{
+			head = newNode;
+		}
+		else
+		{
+			Node * currentNode = head;
+
+			while (currentNode->next != nullptr)
+			{
+				currentNode = currentNode->next;
+			}
+
+			currentNode->next = newNode;
+		}
+
+		size++;
+	}
+
+	void pop_back()
+	{
+		if (head == nullptr)
+		{
+			cout << "linked list is empty" << endl;
+		}
+		else
+		{
+			Node * deleteNode = head;
+			Node * previousNode = nullptr;
+
+			if (size == 1)
+			{
+				head = deleteNode->next;
+
+				delete deleteNode;
+			}
+			else
+			{
+				while (deleteNode->next != nullptr)
+				{
+					previousNode = deleteNode;
+
+					deleteNode = deleteNode->next;
+				}
+
+				previousNode->next = deleteNode->next;
+
+				delete deleteNode;
+			}
+
+			size--;
+		}
+	}
+
+	bool empty()
+	{
+		return (head == nullptr);
+	}
+
+	~List()
+	{
+		while (head != nullptr)
+		{
+			pop_front();
+		}
+	}
 };
 
 int main()
@@ -72,10 +145,16 @@ int main()
 	list.push_front(5);
 	list.push_front(1);
 
+	list.push_back(20);
+	list.push_back(30);
+
 	list.pop_front();
 	list.pop_front();
 	list.pop_front();
-	list.pop_front();
+
+	list.pop_back();
+
+	cout << list.empty() << endl;
 
 	return 0;
 }
