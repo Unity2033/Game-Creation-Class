c122fb031425a9b710a2f8edfe5fae10e9893cbc
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-07-21 21:43:51 +0900

    (2025.7.21) 문자열

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -2,29 +2,7 @@
 
 using namespace std;
 
-template <typename T>
-class List
-{
-private:
-	struct Node
-	{
-		T data;
-		Node * next;
-	};
-
-	int size;
-	Node * head;
-public:
-	List()
-	{
-		size = 0;
-		head = nullptr;
-	}
-
-};
-
 int main()
 {
-
 	return 0;
 }
