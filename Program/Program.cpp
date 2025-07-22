aec2f88f99eb32699f919b624316451eb9a55534
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-07-22 21:44:07 +0900

    (2025.7.22) 스택

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -2,7 +2,38 @@
 
 using namespace std;
 
+template <typename T>
+class Stack
+{
+private:
+	int count;
+	int highset;
+	int capacity;
+
+	T * container;
+public:
+	Stack()
+	{
+		count = 0;
+		highset = -1;
+		capacity = 0;
+
+		container = nullptr;
+	}
+
+	~Stack()
+	{
+		if (container != nullptr)
+		{
+			delete [ ] container;
+		}
+	}
+
+
+};
+
 int main()
 {
+
 	return 0;
 }
