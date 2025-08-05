429916f755195a495423225c5213916bd744a477
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-08-05 21:44:03 +0900

    (2025.8.5) 이진 탐색 트리

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -10,8 +10,13 @@ private:
 	{
 		T data;
 
-		Node * left;
-		Node* right;
+		Node * left = nullptr;
+		Node* right = nullptr;
+
+		Node(T data)
+		{
+			this->data = data;
+		}
 	};
 
 	Node* root;
@@ -21,10 +26,101 @@ public:
 		root = nullptr;
 	}
 
+	void insert(T data)
+	{
+		Node * newNode = new Node(data);
+
+		if (root == nullptr)
+		{
+			root = newNode;
+		}
+		else
+		{
+			Node * currentNode = root;
+
+			while (currentNode != nullptr)
+			{
+				if (currentNode->data == data)
+				{
+					delete newNode;
+
+					return;
+				}
+				else if (currentNode->data > data)
+				{
+					if (currentNode->left == nullptr)
+					{
+						currentNode->left = newNode;
+
+						break;
+					}
+					else
+					{
+						currentNode = currentNode->left;
+					}
+				}
+				else
+				{
+					if (currentNode->right == nullptr)
+					{
+						currentNode->right = newNode;
+
+						break;
+					}
+					else
+					{
+						currentNode = currentNode->right;
+					}
+				}
+			}
+
+		}
+	}
+
+	void release(Node* root)
+	{
+		if (root != nullptr)
+		{
+			release(root->left);
+
+			release(root->right);
+
+			delete root;			
+		}
+	}
+
+	void erase(T data)
+	{
+		Node * currentNode = root;
+
+		while (currentNode != nullptr && currentNode->data != data)
+		{
+			if (currentNode->data > data)
+			{
+				currentNode = currentNode->left;
+			}
+			else
+			{
+				currentNode = currentNode->right;
+			}
+		}
+	}
+
+	~Set()
+	{
+		release(root);
+	}
 };
 
 int main()
 {
+	Set<int> set;
+
+	set.insert(10);
+	set.insert(6);
+	set.insert(20);
+	set.insert(3);
+	set.insert(15);
 
 	return 0;
 }
